2e27056d76f4ff154bd49582f63ee384031711c5
Eko Ginanjar <116570433+Anjar425@users.noreply.github.com>
2024-07-04 01:45:06 +0700

    map clear (#10)

--- app/Models/Coordinate.php
+++ app/Models/Coordinate.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Coordinate extends Model
 {
     use HasFactory;
+
+    protected $table = "coordinates";
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'region_id',
+        'latitude',
+        'longitude',
+    ];
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
